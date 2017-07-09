1b04829570eccec4ca6780ce4f4fb4209b69d76b
Mark Edmondson <github@markedmondson.me>
2017-07-09 09:42:32 +0200

    newdeploy script

--- deploy.sh
+++ deploy.sh
@@ -5,20 +5,23 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 RScript -e 'blogdown::build_site()'
 
-# Go To Public folder
-cd public
+# Copy all over to other GitHub repo that holds public files
+cp -a ~/Documents/markedmondson.me-hugo/public/. ~/Documents/MarkEdmondson1234.github.io/
+
 # Add changes to git.
-git add -A
+#cd ~/Documents/MarkEdmondson1234.github.io/ 
+
+#&& git add -A
 
 # Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
+#msg="rebuilding site `date`"
+#if [ $# -eq 1 ]
+#  then msg="$1"
+#fi
+#git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master
+#git push origin master
 
 # Come Back
-cd ..
+#cd ..
