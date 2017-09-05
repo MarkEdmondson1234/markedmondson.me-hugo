305f0d92fc406478ac5c835536e0b888ecbe6855
Mark IIH <mark@iihnordic.com>
2017-09-05 13:49:59 +0200

    more portable

--- deploy.sh
+++ deploy.sh
@@ -6,22 +6,20 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 RScript -e 'blogdown::build_site()'
 
 # Copy all over to other GitHub repo that holds public files
-cp -a ~/Documents/markedmondson.me-hugo/public/. ~/Documents/MarkEdmondson1234.github.io/
+cp -a public/. ../MarkEdmondson1234.github.io/
 
 # Add changes to git.
-#cd ~/Documents/MarkEdmondson1234.github.io/ 
-
-#&& git add -A
+cd ../MarkEdmondson1234.github.io/ && git add -A
 
 # Commit changes.
-#msg="rebuilding site `date`"
-#if [ $# -eq 1 ]
-#  then msg="$1"
-#fi
-#git commit -m "$msg"
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
 
 # Push source and build repos.
-#git push origin master
+git push origin master
 
 # Come Back
-#cd ..
+cd ../markedmondson.me-hugo
